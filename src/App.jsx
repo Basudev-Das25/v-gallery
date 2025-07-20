26c71f274bffe7f27517363e7bcdfc5a6cbf2dfa
Basudev-Das25 <basudevd983@gmail.com>
2025-07-20 15:07:35 +0530

    intial Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import AppRoutes from './routes';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+  return <AppRoutes/>;
 }
 
 export default App
